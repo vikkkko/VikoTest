3add1a3757099337fe1231f41fe110e210023e61
wowoyinwei <82604458@qq.com>
2019-08-26 16:06:59 +0800

    111

--- VikoTest/Program.cs
+++ VikoTest/Program.cs
@@ -7,9 +7,12 @@ namespace VikoTest
     {
         static void Main(string[] args)
         {
+            string c = args[0];
             Task t = new Task(async ()=> {
                 await Task.Delay(10000);
-                throw new Exception("搞我呢");
+
+                Console.WriteLine(c+"       "+System.DateTime.Now);
+                throw new Exception("error");
             });
             t.Start();
             Console.ReadLine();
