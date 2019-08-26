d4f9fdea782a6c9bb7a8c51cbd44d5781ff2fc05
wowoyinwei <82604458@qq.com>
2019-08-26 11:25:41 +0800

    init

---
+++ VikoTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace VikoTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task t = new Task(async ()=> {
+                await Task.Delay(10000);
+                throw new Exception("搞我呢");
+            });
+            t.Start();
+            Console.ReadLine();
+        }
+    }
+}
